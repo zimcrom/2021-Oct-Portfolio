4622e3d85644620158b5c8ee447fff5009a4189b
Megan Henning <megannh6349@gmail.com>
2021-10-05 12:44:14 -0400

    added logic to make navbar mobile friendly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 // Dropdown
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems);
+    var instances = M.Dropdown.init(elems, "closeOnClick");
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, "closeOnClick");
   });
