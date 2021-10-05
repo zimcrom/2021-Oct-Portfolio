189dc460a61866d28d1619eaa580614affaff247
Megan Henning <megannh6349@gmail.com>
2021-10-05 14:44:48 -0400

    added logic for picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Dropdown.init(elems, "closeOnClick");
   });
 
-
+// Mobile dropdown
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, "closeOnClick");
+  });
+
+  // Photo logic
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
   });
